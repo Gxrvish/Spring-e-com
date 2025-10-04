82d96de0630f400de555f652f928ff48dc795e9e
Gxrvish <garvishpanchal004@gmail.com>
2025-10-04 14:41:25 +0530

    [ADD] add delte initial code

--- src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
@@ -11,6 +11,7 @@ import com.ecommerce.sb_ecom.model.Category;
 public class CategoryServiceImpl implements CategoryService {
 
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -19,7 +20,21 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+            .filter(c -> c.getCategoryId().equals(categoryId))
+            .findFirst()
+            .orElse(null);
+        if (category != null) {
+            categories.remove(category);
+            return "Category with categoryId: " + categoryId + " deleted successfully";
+        }
+        return "Deleting categoryId: " + categoryId + " Failed.";
     }
 
 }
