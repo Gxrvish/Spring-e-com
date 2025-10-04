4ec2875d8d4035648d691c3c520e420c27ce67c7
Garvish Panchal <garvishpanchal004@gmail.com>
2025-10-04 13:09:04 +0530

    [ADD] starting files

---
+++ src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.ecommerce.sb_ecom.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.sb_ecom.model.Category;
+import com.ecommerce.sb_ecom.service.CategoryService;
+
+@RestController
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategoryString(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "Category added successfully!";
+    }
+
+}
---
+++ src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -0,0 +1,35 @@
+package com.ecommerce.sb_ecom.model;
+
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+    
+
+    /**
+     * @return Long return the categoryId
+     */
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    /**
+     * @param categoryId the categoryId to set
+     */
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    /**
+     * @return String return the categoryName
+     */
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    /**
+     * @param categoryName the categoryName to set
+     */
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
