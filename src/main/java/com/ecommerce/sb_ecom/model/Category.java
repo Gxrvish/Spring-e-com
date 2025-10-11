c966762110f513a2674231091c8a6708e1baf939
Gxrvish <garvishpanchal004@gmail.com>
2025-10-11 19:57:06 +0530

    Add custom exception and validation message

--- src/main/java/com/ecommerce/sb_ecom/model/Category.java
+++ src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
-    @NotBlank
+    @NotBlank(message = "Category name must not be blank")
+    @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
 }
